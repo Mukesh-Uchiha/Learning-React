b7e69138fde065e736d0f6d9d029a623b6d28ed8
Mukesh N <96813673+Mukesh-Uchiha@users.noreply.github.com>
2023-02-27 16:44:12 +0530

    Add files via upload

---
+++ usestate-hook/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    if (count > 0) setCount(count - 1);
+  }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
